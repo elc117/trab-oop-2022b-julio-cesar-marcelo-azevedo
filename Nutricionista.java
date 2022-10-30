f99d07ed1c2abd43e9f80549383dbbc3e9ad2f90
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2022-10-30 20:52:38 -0300

    Add files via upload

--- Nutricionista.java
+++ Nutricionista.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-public class Nutricionista{
+public class Nutricionista extends Aluno{
 	
 	/*
 	 Para cada 1g de carboidratos, multiplicar por 4 calorias;
@@ -23,8 +23,23 @@ public class Nutricionista{
 	}
 	
 	
-	public void criaDieta() {
+	public void criaDieta(int totAlunos) {
+		Aluno al = new Aluno();
+		// BEE : Gasto energetico basal
+	
+		//Crianças e adolescentes com peso normal (Meninos)
+		//BEE_MENINOS = 68 – (43,3 x idade [anos]) + (712 x altura [m]) + (19,2 x peso [kg]);
+		double BEE_MENINOS = 68 - (43.3 * al.getIdade() + (712 * al.getAltura() + (19.2 * al.getPeso()))) ;
 		
+		//Crianças e adolescentes com peso normal (Meninas)
+		//BEEMENINAS = 189 – (17,6 x idade [anos]) + (625 x altura [m]) + (7,9 x peso [kg]);
+
+		//Adolescentes com sobrepeso e obesidade (Homens)
+		//BEE-HOMENS = 419,9 – (35,5 x idade [anos]) + (418,9 x altura [m]) + (16,7 x peso [kg]);
+		
+		//Adolescentes com sobrepeso e obesidade (Mulheres)
+		//BEE-MULHERES = 515,8 – (26,8 x idade [anos]) + (347 x altura [m]) + (12,4 x peso [kg]);
+
 	}
 	
 	public void editaDieta() {
