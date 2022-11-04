3b8bdd50289afa97b3cc4a2a47f1300c3a0f2684
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2022-11-04 11:09:27 -0300

    Add files via upload

--- Escola.java
+++ Escola.java
@@ -7,16 +7,12 @@ public class Escola extends Aluno{
     private String tipoAlimento;
     private ArrayList<Escola> despensa;
     private ArrayList<Aluno> alunosGeral;
-    //private ArrayList<AlunoEspecifico> alunosEspecificos;
+    private ArrayList<Aluno> alunosEspecificos;
 
-    public Escola(){
+    public Escola(String nome){
+    	this.nome = nome;
     }
     
-    public Escola(String name) {
-    	this.nome = nome;
-    	this.alunosGeral = new ArrayList<Aluno>();
-    	//this.alunosEspecificos = new ArrayList<Aluno>();
-    }
     
     public void setNome(String nome) {
     	this.nome = nome;
@@ -45,24 +41,55 @@ public class Escola extends Aluno{
         return this.totAlunos;
     }
     
-    public void addAlunoGeral(Aluno alunoGeral) {
-    	this.alunosGeral.add(alunoGeral);
+    
+    public void addDespensa(ArrayList<Escola> despensa) {
+    	this.despensa.addAll(despensa);
     	
     }
     
-    /*public void addAlunoEspecifico(Aluno alunoEspecifico) {
-    	this.alunosEspecificos.add(alunoEspecifico);
-    }
-    
-  	
-    
-    public ArrayList<String> getAlunosInfo(){
-    	ArrayList<String> alunos = new ArrayList<String>();
+    public static void main(String[] args) {
+    	Escola e = new Escola("Margarida Lopes");
     	
-    	for (Aluno al: aluno) {
+    	ArrayList<Aluno> alunosGeral = new ArrayList<Aluno>();
+    	ArrayList<Aluno> alunosEspecificos = new ArrayList<Aluno>();
+    	
+    	ArrayList<Escola> despensa = new ArrayList<Escola>();
+    
+    	
+    	Aluno a1 = new Aluno("Joao", 12, 45f, 1.45f, false);
+    	Aluno a2 = new Aluno("Nicolas", 13, 41f, 1.41f, false);
+  
+    	Aluno a3 = new Aluno("Paula", 14, 43f, 1.42f, true);
+    	Aluno a4 = new Aluno("Pedro", 11, 40f, 1.47f, true);
+    	        	
+    	alunosGeral.add(a1);
+    	alunosGeral.add(a2);
+    	
+    	alunosEspecificos.add(a3);
+    	alunosEspecificos.add(a4);
+    	
+    	
+    	
+    	System.out.println("Lista alunos gerais: "); 
+    	for (Aluno al : alunosGeral) {
     		
+    		
+    		System.out.println(al.getNome());
+
     	}
-    	return alunos;
+    		System.out.println("----------------------");
+    	
+    	System.out.println("Lista alunos especificos: ");
+    	for (Aluno al : alunosEspecificos) {
+    		
+    		
+    		
+    		System.out.println(al.getNome());
+    		
+
+    	}
+		System.out.println("----------------------");
+
+    	
     }
-    */
 }
