99563966c42c19d0922cbd5cb6de9a065becb0aa
mdazevedo96 <42869269+mdazevedo96@users.noreply.github.com>
2022-11-09 19:36:06 -0300

    Add files via upload

--- Aluno.java
+++ Aluno.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class Aluno{
     private String nomeAluno;
     private int idade;
     private float peso;
     private float altura;
     private boolean dietaRestrita;
+    private String tipoDieta;
+    
 
 public Aluno(){
     this.nomeAluno = "Joao";
@@ -19,6 +23,7 @@ public Aluno(String nomeAluno, int idade, float peso, float altura, boolean diet
     this.peso = peso;
     this. altura = altura;
     this.dietaRestrita = dietaRestrita;
+    this.setTipoDieta(dietaRestrita);
 }
 
 public void setNome(String nomeAluno){
@@ -28,6 +33,7 @@ public void setNome(String nomeAluno){
 public String getNome(){
     return this.nomeAluno;
 }
+
 
 public void setPeso(float peso){
     this.peso = peso;
@@ -53,14 +59,39 @@ public int getIdade(){
     return this.idade;
 }
 
-public String getContactInfo() {
+public void setDietaRestritiva(boolean dietaRestrita) {
+	this.dietaRestrita = dietaRestrita;
+}
+
+public boolean getDietaRestritiva() {
+	return this.dietaRestrita;
+}
+
+public void setTipoDieta(boolean dieta) {
+	if (this.dietaRestrita == true) {
+		this.tipoDieta = "Obesidade";
+	}
+	else {
+		this.tipoDieta = "Normal";
+	}
+}
+
+
+public String getTipoDieta() {
+	return tipoDieta;
+}
+
+public String getInfoAluno() {
     return "{" +
       "class='Aluno'" +
       ", nome='" + getNome() + "'" +
       ", idade='" + getIdade() + "'" +
       ", peso='" + getPeso() + "'" +
       ", altura='" + getAltura() + "'" +
+      ", dietaRestritiva='" + getDietaRestritiva() + "'" + 
+      ", tipoDieta='" + getTipoDieta() + "'" +
       "}";
   }
+
 
 }
--- AlunoTest.java
+++ AlunoTest.java
@@ -1,22 +1,15 @@
-import java.util.ArrayList;
 
-public class AlunoTest extends Aluno{
+public class AlunoTest extends Aluno {
 
-    public static void main(String[] args){
-    	
-    	ArrayList<Aluno> listaAlunos = new ArrayList<Aluno>();
-        Aluno a1 = new Aluno("Joao", 12, 45.0f, 1.50f, false);
-        Aluno a2 = new Aluno("Paula", 15, 43.0f, 1.450f, true);
-        Aluno a3 = new Aluno ("Nicolas", 12, 41.0f, 1.55f, false);
-        Aluno a4 = new Aluno ("Natalia", 13, 40.0f, 1.42f, true);
-        
-        listaAlunos.add(a1);
-        listaAlunos.add(a2);
-        listaAlunos.add(a3);
-        listaAlunos.add(a4);
-        
-        
-    }
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno("Joao", 12, 54f, 1.54f, true);
+		
+		Aluno a2 = new Aluno("Pedro", 14, 40f, 1.60f, false);
+		
+		System.out.println(a1.getInfoAluno());
+		System.out.println(a2.getInfoAluno());
 
+
+	}
 
 }
